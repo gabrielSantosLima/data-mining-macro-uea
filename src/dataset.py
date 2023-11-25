a0e06a906515acf691f5d02cacbbe93eef219afe
Gabriel Lima <lima.gabrieldsantos@gmail.com>
2023-11-25 15:20:31 -0400

    feat: add dataset building

--- src/dataset.py
+++ src/dataset.py
@@ -1,6 +1,65 @@
 
+from os import listdir
+from os.path import join
+from re import findall, sub
 
-def main():
-    pass
+from pypdf import PdfReader
 
-main()
+# [X] Ler todos os PDFs
+# [X] Extrair as tabelas de notas
+# [X] Criar um CSV com todas as notas
+
+def get_pdf_content(pdf_path: str) -> list[str]:
+    print(f"reading {pdf_path}")
+    reader = PdfReader(pdf_path)
+    
+    candidates = []
+    for page in reader.pages:
+        text = page.extract_text()
+        # find all table rows by a regex expression 
+        full_texts = findall("(?=([0-9]{7}-[0-9]{1} [ \w]+ [0-9]{2}\/[0-9]{2}\/[0-9]{2}( +[0-9]+(,[0-9]+)?)+))", text)
+
+        for full_text in full_texts:
+            candidate: str = full_text[0]
+            candidate = candidate.strip().replace(',', '.').replace('  ', ',')
+            candidate = sub("\s(?=[0-9])", ',', candidate)
+            candidate = candidate.replace(' ', ',', 1)
+            candidates.append(f"{candidate}\n")
+
+    return candidates
+
+def get_pdf_paths(pdf_folder_path: str) -> list[str]:
+    pdf_folder = listdir(pdf_folder_path)
+    pdfs: list[str] = []
+    
+    for pdf_folder in pdf_folder:
+        pdfs_by_year_path = join(pdf_folder_path, pdf_folder) # default_path + "year"
+        pdfs_by_year = listdir(pdfs_by_year_path)
+        for pdf in pdfs_by_year:
+            pdf_path = join(pdfs_by_year_path, pdf) # default_path + "year" + "pdf filename"
+            pdfs.append(pdf_path) # save file paths
+    
+    return pdfs
+
+def dataset(pdf_folder_path: str) -> str:
+    csv_path = join('.', 'data', 'csv')
+    filename = 'data.csv'
+    out_path = join(csv_path, filename)
+
+    if filename in listdir(csv_path):
+        print('data.csv already exists.')
+        return out_path
+
+    pdfs = get_pdf_paths(pdf_folder_path)
+    csv_file = open(out_path, 'a+')
+
+    header = "numinscricao,nome,datanasc,classificao,NF,CG,ENEM,nota1,disc1,disc2,disc3,CE,RED,nota2,OP1,OP2,OP3\n"
+    csv_file.write(header)
+    
+    for pdf in pdfs:
+        pdf_content = get_pdf_content(pdf)
+        csv_file.write(" ".join(pdf_content))
+
+    csv_file.close()
+
+    return out_path
